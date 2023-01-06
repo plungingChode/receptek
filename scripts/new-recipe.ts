e038aaa9f13122a4ade256c51b13229044586b19
plungingChode <szigeti.peter23@gmail.com>
2023-01-06 19:12:58 +0100

    update `create:recipe` command
    Add `readline-sync` to simplify input reading.
    Request user input, if parameters were not provided.

--- scripts/new-recipe.ts
+++ scripts/new-recipe.ts
@@ -1,31 +1,52 @@
 import process from "process";
 import fs from "fs";
 import path from "path";
+import readline from "readline-sync";
+import { deaccent } from "../src/util.js";
 
 const TEMPLATE_FILE = "recipe-template.md";
 const RECIPES_DIR = "src/pages/recept";
 
 const usage = `
 Usage: pnpm create:recipe <name> [source]
+       pnpm create:recipe --help
+       pnpm create:recipe
 
+Generate a new recipe from the common template. Omitting the required parameters
+will prompt the user for them instead.
+
+Options:
+  -h, --help   - Print this message.
+  
 Positional arguments:
-  * name - The name of the recipe. This will also be used to generate the file name. 
+  * name   - The name of the recipe. This will also be used to generate the file name. 
   * source - The source of the recipe. A URL, image name or source description.
 `;
 
 main();
 
-function main() {
+async function main() {
   const args = process.argv.slice(2);
   const cwd = process.cwd();
 
-  if (args.length < 1) {
+  if (args[0] === "-h" || args[0] === "--help") {
     console.log(usage);
-    process.exit(1);
+    process.exit(0);
+  }
+
+  if (args.length < 1) {
+    const params = await getParameters();
+    args[0] = params.name;
+    args[1] = params.source;
   }
 
   const name = args[0] as string;
   const source = `# forrás: ${args[1] || ""}`;
+
+  if (!name) {
+    console.log(usage);
+    process.exit(1);
+  }
 
   // Replace placeholder fields with concrete values
   const template = fs.readFileSync(TEMPLATE_FILE, "utf-8");
@@ -36,13 +57,12 @@ function main() {
   const outFile = path.join(cwd, RECIPES_DIR, fileName);
   fs.writeFileSync(outFile, content, { encoding: "utf-8", flag: "w" });
 
-  console.log(`Created ${outFile}`);
+  console.log(`✓ Created ${outFile}`);
 }
 
-
 function slugify(s: string): string {
-  const normalized = s.toLocaleLowerCase().normalize("NFD");
-  return normalized
+  return deaccent(s)
+    .toLocaleLowerCase()
     .trim()
     .replace(/[^\w\s-]/g, "")
     .replace(/[\s_-]+/g, "-")
@@ -65,4 +85,12 @@ function toFileName(existingRecipes: string[], s: string): string {
     slug = originalSlug + "-" + nCopies;
   }
   return slug + ".md";
+}
+
+async function getParameters(): Promise<{ name: string; source: string }> {
+  console.log("Enter recipe details. Fields marked with '*' are required.");
+  const name = readline.question("[*] Recipe name: ");
+  const source = readline.question("[ ] Recipe source: ");
+  console.log();
+  return { name, source };
 }
