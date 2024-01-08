aae27cfb162ba6e814ab0d2018d9a778ad0a6610
Péter SZIGETI <p.szigeti@adiumsoft.hu>
2024-01-08 20:44:17 +0100

    finalize shopping list (for now)

--- src/components/shopping-list/ShoppingList.svelte
+++ src/components/shopping-list/ShoppingList.svelte
@@ -4,9 +4,17 @@ import { persistentShoppingList as shoppingList } from "./store";
 
 import ShoppingListDefault from "./ShoppingListDefault.svelte";
 import ShoppingListOverview from "./ShoppingListOverview.svelte";
+
+// TODO finish overview mode (add delete functionality)
+// TODO add ability to add arbitrary entries
+// TODO add message when empty
 </script>
 
 <main class="max-w-lg mx-auto p-4">
-  <!-- <ShoppingListDefault store={shoppingList} /> -->
-  <ShoppingListOverview store={shoppingList} />
+  {#if $shoppingList.length === 0}
+    <em>A bevásárlólista üres</em>
+  {:else}
+    <ShoppingListDefault store={shoppingList} />
+    <!-- <ShoppingListOverview store={shoppingList} /> -->
+  {/if}
 </main>
